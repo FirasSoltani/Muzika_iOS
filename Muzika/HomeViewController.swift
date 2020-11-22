734ded5664eb30858094cd5ef47b0b937747bba1
firas <firas.soltani@esprit.tn>
2020-11-22 18:24:04 +0100

    Started working on interfaces , Profile interface , Featured , Home.

--- Muzika/HomeViewController.swift
+++ Muzika/HomeViewController.swift
@@ -8,11 +8,68 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController , UIScrollViewDelegate  {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var tableHeight: NSLayoutConstraint!
+    
+    
+    @IBOutlet weak var headerView: UIView!
+    
+    
+    
+    
+    var images: [String] = ["Lofi","Beatles","Alan Watts"]
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        for index in 0..<images.count {
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+
+            let imageView = UIImageView(frame: frame)
+            imageView.image = UIImage(named: images[index])
+            self.scrollView.addSubview(imageView)
+        }
+       
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    
+    
+}
+
+extension HomeViewController : UITableViewDataSource , UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let playlistCell = tableView.dequeueReusableCell(withIdentifier: "playlistCell")
+        let contentview = playlistCell?.contentView
+        let imageView = contentview?.viewWithTag(1) as! UIImageView
+        let label = contentview?.viewWithTag(2) as! UILabel
+        
+        imageView.image = UIImage(named: "Lofi")
+        label.text = "Lofi playlist"
+        
+        return playlistCell!
     }
 }
+
+
 
--- Muzika/ProfileViewController.swift
+++ Muzika/ProfileViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+   
 
+    @IBOutlet weak var liked: UIView!
+    @IBOutlet weak var logout: UIView!
+    @IBOutlet weak var favorites: UIView!
+    @IBOutlet weak var playlists: UIView!
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var profileImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+     
+        profileImage.layer.masksToBounds = true
+        profileImage.layer.cornerRadius = profileImage.bounds.width / 2
+        cardView.layer.cornerRadius = 50
+        favorites.layer.cornerRadius = 30
+        logout.layer.cornerRadius = 30
+        playlists.layer.cornerRadius = 30
+        liked.layer.cornerRadius = 30
         // Do any additional setup after loading the view.
     }
     
@@ -28,3 +42,5 @@ class ProfileViewController: UIViewController {
     */
 
 }
+
+
